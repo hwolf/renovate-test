8914ec8cf6650a49301205b398ec8c6eb2206233
hwolf <heiko>
2021-12-13 22:17:19 +0100

    Initial commit

---
+++ src/main/java/hwolf/renovate/RenovateTestApplication.java
@@ -0,0 +1,13 @@
+package hwolf.renovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenovateTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hwolf/renovate/RenovateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hwolf.renovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
